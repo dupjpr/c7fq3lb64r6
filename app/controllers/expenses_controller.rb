f376dcff2a87e2512c9b51df74735dc1b1e3a75e
pakman <pakman@DESKTOP-J81E3KF.localdomain>
2020-07-25 11:06:33 -0500

    add like to the controller

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -2,7 +2,8 @@ class ExpensesController < ApplicationController
   def index
     @expenses = Expense.order("date DESC")
     if params[:concept].present?
-      @expenses = @expenses.where("lower(concept) = ?", params[:concept].downcase)
+      @expenses = @expenses.where("concept LIKE ?", "%#{params[:concept]}%")
+      # @expenses = @expenses.where("lower(concept) = ?", params[:concept].downcase)
     end
     if params[:category_id].present?
       @expenses = @expenses.where(category_id: params[:category_id])
